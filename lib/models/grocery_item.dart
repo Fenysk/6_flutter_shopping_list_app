e6934e44cba08f5df6a11a921bdce2594e9db16e
Fenysk <minefenysk@gmail.com>
2024-05-26 14:32:20 +0200

    🚧 Add uuid dependency and generate unique IDs for GroceryItems

--- lib/models/grocery_item.dart
+++ lib/models/grocery_item.dart
@@ -1,12 +1,16 @@
 import 'package:shopping_list_app/models/category.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+String get generatedId => 'grcr_${uuid.v4().trim().replaceAll('-', '').substring(0, 8)}';
 
 class GroceryItem {
-  const GroceryItem({
-    required this.id,
+  GroceryItem({
     required this.name,
     required this.quantity,
     required this.category,
-  });
+  }) : id = generatedId;
 
   final String id;
   final String name;
